fca213ba43d567b9428cf4719c927143878b43f9
Charles Carter <ccc31807@yahoo.com>
2020-10-09 08:08:36 -0400

    ch 23 quiz solution

---
+++ quizzes/ch23-single.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace csharp_Q23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# quiz 23 - single threaded");
+            Console.Write("Enter the highest number to find primes: ");
+            string input = Console.ReadLine();
+            long findTo = long.Parse(input);
+            List<long> primes = new List<long>();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (long i = 1; i < findTo; i++)
+            {
+                bool foundP = isPrime(i);
+                //Console.WriteLine($"from foundP: i is {i}");
+                if (foundP)
+                    primes.Add(i);
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+
+            //output
+            Console.WriteLine("PRINTING PRIMES");
+            //foreach (int element in primes)
+            //    Console.Write($" {element} ");
+            Console.WriteLine($"\n\nfound primes in time {ts}");
+        }
+
+        private static bool isPrime(long n)
+        {
+            bool isP = true;
+            for (int i = 2; i <= (int)Math.Sqrt(n); i++)
+                if (n % i == 0)
+                {
+                    isP = false;
+                    break;
+                }
+            return isP;
+        }
+    }
+}
